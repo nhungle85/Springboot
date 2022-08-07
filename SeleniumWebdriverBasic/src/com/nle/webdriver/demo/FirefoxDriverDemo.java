7d3ef9278c5c7f048ecdd2d456c932d2f5aea0d6
nhungle <misssg85@gmail.com>
2022-08-07 15:24:04 -0500

    Formate ChromeDriverDemo class Add FirefoxDriverDemo class

--- SeleniumWebdriverBasic/src/com/nle/webdriver/demo/FirefoxDriverDemo.java
+++ SeleniumWebdriverBasic/src/com/nle/webdriver/demo/FirefoxDriverDemo.java
@@ -1,24 +1,25 @@
 package com.nle.webdriver.demo;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ChromeDriverDemo {
+public class FirefoxDriverDemo {
 
 	public static void main(String[] args) {
 		// Have to download chromedriver match with your current chrome version in your
 		// computer
-		System.setProperty("webdriver.chrome.driver", "/Users/nhungle/Documents/Study/Webdrivers/chromedriver");
+		System.setProperty("webdriver.gecko.driver", "/Users/nhungle/Documents/Study/Webdrivers/geckodriver");
 
 		// In order to create scripts that interact with the Selenium Server (Remote
 		// WebDriver)
 		// or create local Selenium WebDriver scripts, you need to make use of
 		// language-specific client drivers
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 
 		driver.manage().window().maximize();
 
 		driver.get("https://automationtesting.co.uk/");
+
 	}
 
 }
